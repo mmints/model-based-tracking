bd361fff79a9a5b1d2b7dcc423289aa669f94037
Mark Oliver Mints <mmints@uni-koblenz.de>
2019-12-23 07:07:45 +0100

    [Add] Error handling for cuda during compile time

---
+++ libraries/ErrorHandling/HANDLE_CUDA_ERROR.h
@@ -0,0 +1,16 @@
+#ifndef HANDLE_CUDA_ERROR_H
+#define HANDLE_CUDA_ERROR_H
+
+static void handleCudaError( cudaError_t err,
+                             const char *file,
+                             int line ) {
+    if (err != cudaSuccess) {
+        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+                file, line );
+        exit( EXIT_FAILURE );
+    }
+}
+
+#define HANDLE_CUDA_ERROR( err ) (handleCudaError( err, __FILE__, __LINE__ ))
+
+#endif //HANDLE_CUDA_ERROR_H
