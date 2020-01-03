3f1ad0b0778c8130b831cbd44b551cf633ac51cd
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-01-03 12:44:31 +0100

    [Update] Move GL initialization into separated function

--- executables/TEST_OpenGL_Texture_into_CUDA/main.cpp
+++ executables/TEST_OpenGL_Texture_into_CUDA/main.cpp
@@ -7,8 +7,8 @@
 
 #include "kernel.h"
 
-#define WIDTH 100
-#define HEIGHT 100
+#define WIDTH 1280
+#define HEIGHT 720
 
 using namespace sl;
 
@@ -18,7 +18,22 @@ GLFWwindow* window;
 Camera zed;
 Mat gpuLeftImage;
 Mat outImage = Mat(WIDTH, HEIGHT, MAT_TYPE_8U_C4, MEM_GPU);
+cudaGraphicsResource* pcuImageRes; // Cuda resources for CUDA-OpenGL interoperability
 
+void initGL()
+{
+    glfwInit();
+    CVK::useOpenGL33CoreProfile();
+    window = glfwCreateWindow(WIDTH, HEIGHT, "[TEST] OpenGL Texture into CUDA", nullptr, nullptr);
+    glfwSetWindowPos( window, 100, 50);
+    glfwMakeContextCurrent(window);
+    glewInit();
+    glEnable(GL_DEPTH_TEST);
+
+    CVK::State::getInstance()->setBackgroundColor(BLACK);
+    glm::vec3 BgCol = CVK::State::getInstance()->getBackgroundColor();
+    glClearColor( BgCol.r, BgCol.g, BgCol.b, 0.0);
+}
 
 // Set up all necessary shader and use the Particle Generator to create a geometry, that will be
 // rendered into tha Color Buffer of ang given FBO
@@ -52,7 +67,7 @@ void renderIntoFBO(CVK::FBO &fbo, int particle_count)
 }
 
 // Renders the first Color Buffer of the given FBO onto a screen filling quad.
-void renderToScreen(GLuint textureID)
+void renderTextureToScreen(GLuint textureID)
 {
     const char *shadernamesSimpleTexture [ 2 ] = { SHADERS_PATH "/ScreenFill.vert", SHADERS_PATH "/SimpleTexture.frag" };
     CVK::ShaderSimpleTexture simpleTextureShader( VERTEX_SHADER_BIT | FRAGMENT_SHADER_BIT, shadernamesSimpleTexture );
@@ -67,24 +82,12 @@ void renderToScreen(GLuint textureID)
 
 int main()
 {
-    glfwInit();
-    CVK::useOpenGL33CoreProfile();
-    window = glfwCreateWindow(WIDTH, HEIGHT, "[TEST] OpenGL Texture into CUDA", nullptr, nullptr);
-    glfwSetWindowPos( window, 100, 50);
-    glfwMakeContextCurrent(window);
-    glewInit();
-    glEnable(GL_DEPTH_TEST);
-
-    CVK::State::getInstance()->setBackgroundColor(BLACK);
-    glm::vec3 BgCol = CVK::State::getInstance()->getBackgroundColor();
-    glClearColor( BgCol.r, BgCol.g, BgCol.b, 0.0);
+    initGL();
 
     CVK::FBO fbo( WIDTH, HEIGHT, 1, true );
     renderIntoFBO(fbo, 1);
 
     // CUDA interopertion part
-    //HANDLE_CUDA_ERROR(cudaGLSetGLDevice(0));
-
     struct cudaGraphicsResource *tex_resource;
     HANDLE_CUDA_ERROR(cudaGraphicsGLRegisterImage(&tex_resource, fbo.getColorTexture(0), GL_TEXTURE_2D, cudaGraphicsMapFlagsReadOnly));
     HANDLE_CUDA_ERROR(cudaGraphicsMapResources(1, &tex_resource));
@@ -93,14 +96,11 @@ int main()
     HANDLE_CUDA_ERROR(cudaGraphicsSubResourceGetMappedArray(&tex_array, tex_resource, 0, 0));
     HANDLE_CUDA_ERROR(cudaGraphicsUnmapResources(1, &tex_resource));
 
-
-
+    renderTextureToScreen(fbo.getColorTexture(0));
+    callKernel(WIDTH, HEIGHT, tex_array);
 
     while(!glfwWindowShouldClose( window))
     {
-
-        renderToScreen(fbo.getColorTexture(0));
-
         glfwSwapBuffers( window);
         glfwPollEvents();
     }
