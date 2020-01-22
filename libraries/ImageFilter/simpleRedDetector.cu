fb313567e3accecb290475346e69aececba2b6fb
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-01-22 13:19:11 +0100

    [Refactor] Return a RGB coded red color map

--- libraries/ImageFilter/simpleRedDetector.cu
+++ libraries/ImageFilter/simpleRedDetector.cu
@@ -2,18 +2,18 @@
 
 #include "ImageFilter.h"
 
-
 __global__ void simpleRedDetectorKernel(sl::uchar4 *d_in, sl::uchar4 *d_out, size_t step, unsigned char threshold) {
 
-    // Flat the 2D Coordinates
+    // Flat the 2D Coordinates to 1D
     uint32_t zed_x = threadIdx.x + blockIdx.x * blockDim.x;
     uint32_t zed_y = threadIdx.y + blockIdx.y * blockDim.y;
-    uint32_t offset = zed_x + zed_y * step; // Flat coordinate to memory space
+    uint32_t offset = zed_x + zed_y * step;
 
     sl::uchar4 pixel_color = d_in[offset];
-    sl::uchar4 red = sl::uchar4(0, 0, 255, 0);
+    sl::uchar4 red = sl::uchar4(255, 0, 0, 0);
 
-    if (pixel_color.z >= threshold && pixel_color.y < 100 && pixel_color.x < 100) { // BGR to RGB
+    // The values from ZED are coming in BGR
+    if (pixel_color.z >= threshold && pixel_color.y < 100 && pixel_color.x < 100) {
         d_out[offset] = red;
     }
     else {
@@ -21,6 +21,7 @@ __global__ void simpleRedDetectorKernel(sl::uchar4 *d_in, sl::uchar4 *d_out, siz
     }
 }
 
+// Returns a red color map in d_out
 void kernel::simpleRedDetector(sl::uchar4 *d_in, sl::uchar4 *d_out, unsigned char threshold, int width, int height, size_t step)
 {
     const size_t BLOCKSIZE_X = 32;
@@ -32,5 +33,6 @@ void kernel::simpleRedDetector(sl::uchar4 *d_in, sl::uchar4 *d_out, unsigned cha
     dimGrid.x = (width + dimBlock.x - 1) / dimBlock.x;
     dimGrid.y = (height + dimBlock.y - 1) / dimBlock.y;
 
+    // TODO: Add HSV transformation kernel to remove lightning artifacts
     simpleRedDetectorKernel<<<dimGrid, dimBlock>>>(d_in, d_out, step, threshold);
 }
