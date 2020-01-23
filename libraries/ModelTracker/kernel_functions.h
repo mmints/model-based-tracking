c87c1183675629a61750933525bb7ba9d3c0da91
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-01-23 23:30:58 +0100

    [Add] Likelihood kernel that scores the avaiablility of red color in particles and input image

--- libraries/ModelTracker/kernel_functions.h
+++ libraries/ModelTracker/kernel_functions.h
@@ -13,6 +13,17 @@ namespace mt
 {
     // testing kernel that was used in the deprecated version of the tracker
     void testCallKernel(sl::uchar4 *zed_in, sl::uchar4 *zed_out,  size_t step, int width, int height, cudaArray *particle_grid_tex_array, float *dev_global_weight_memory);
+
+    /**
+     * Compare pixel pairwise from the particle_grid_texture and corresponding zed_in.
+     * Return the weights of the particles in dev_global_weight_memory
+     */
+    void compareRedPixel(sl::uchar4 *zed_in,  size_t step,
+                         int particle_scale, int particle_grid_dimension,
+                         int particle_width, int particle_height,
+                         cudaArray *particle_grid_tex_array,
+                         float *dev_global_weight_memory,
+                         sl::uchar4 *debug_img_out); // DEBUG IMG
 }
 
 #endif //MT_KERNEL_FUNCTIONS_H
---
+++ libraries/ModelTracker/kernels/compareRedPixel.cu
@@ -0,0 +1,88 @@
+#include "ModelTracker/kernel_functions.h"
+#include <ErrorHandling/HANDLE_CUDA_ERROR.h>
+
+
+texture<uchar4, 2, cudaReadModeElementType> particle_grid_texture_ref;
+
+// Compare the pixel values of the particle and the zed
+__device__ void compare(float &weight, const uchar4 &particle_pixel, const sl::uchar4 &zed_pixel)
+{
+    if (particle_pixel.x == zed_pixel.x) //&& particle_pixel.y == zed_pixel.y && particle_pixel.z == zed_pixel.z)
+        weight = 1.f;
+    else
+        weight = 0.f;
+}
+
+__global__ void compareRedPixelKernel(sl::uchar4 *zed_in, size_t step,
+                                int particle_scale, int particle_grid_dimension,
+                                int particle_width, int particle_height,
+                                float *global_weight_memory,
+                                sl::uchar4 *debug_img_out)
+{
+    // Get the texel value from particleGrid.texture (parts as particle_grid_texture_ref)
+    // use unsigned integer because the numbers can become very large
+    uint32_t particle_grid_texture_x = threadIdx.x + blockIdx.x * blockDim.x;
+    uint32_t particle_grid_texture_y = threadIdx.y + blockIdx.y * blockDim.y;
+
+    // Transfer particle grid pixel coordinate to ZED pixel coordinate
+    uint32_t zed_x = (particle_grid_texture_x % particle_width) * particle_scale;
+    uint32_t zed_y = (particle_grid_texture_y % particle_height) * particle_scale;
+    uint32_t offset = zed_x + zed_y * step; // Flat coordinate to memory space
+
+    float weight = 0.f;
+
+    // Calculate the index of the current corresponding particle to the given texel
+    int particle_index = (int)(particle_grid_texture_x / particle_width) + (int)(particle_grid_texture_y / particle_height) * particle_grid_dimension;
+
+    // If the RED channel of the RedMapMat is
+    if (zed_in[offset].x != 0)
+    {
+        uchar4 particle_grid_pixel_value = tex2D(particle_grid_texture_ref, particle_grid_texture_x, particle_grid_texture_y);
+        if (particle_grid_pixel_value.x != 0)
+            printf("%i \n", particle_grid_pixel_value.x);
+
+        debug_img_out[offset] = sl::uchar4(0, 0, 0, 0);
+
+        compare(weight, particle_grid_pixel_value, zed_in[offset]);
+
+        //Debug: Have a look how the particle are spread on the input img
+        if (weight > 0.f) {
+            debug_img_out[offset] = sl::uchar4(0, 255, 0, 0);
+        }
+        else {
+            debug_img_out[offset] = sl::uchar4(0, 0, 0, 0);
+        }
+    }
+
+    atomicAdd(&global_weight_memory[particle_index], weight);
+}
+
+void mt::compareRedPixel(sl::uchar4 *zed_in,  size_t step,
+                        int particle_scale,
+                        int particle_grid_dimension,
+                        int particle_width, int particle_height,
+                        cudaArray *particle_grid_tex_array,
+                        float *dev_global_weight_memory,
+                         sl::uchar4 *debug_img_out)
+{
+    HANDLE_CUDA_ERROR(cudaBindTextureToArray(particle_grid_texture_ref, particle_grid_tex_array));
+
+    // Given Code
+    const size_t BLOCKSIZE_X = 32; // Max Threads in X
+    const size_t BLOCKSIZE_Y = 8;  // Max Threads in Y
+
+    dim3 dimBlock{BLOCKSIZE_X,BLOCKSIZE_Y}; // Threads
+    dim3 dimGrid; // Blocks
+
+    // Launch enough blocks for the requestet amount of threads
+    dimGrid.x = (particle_width * particle_grid_dimension + dimBlock.x - 1) / dimBlock.x;
+    dimGrid.y = (particle_height * particle_grid_dimension + dimBlock.y - 1) / dimBlock.y;
+
+    compareRedPixelKernel<<<dimGrid, dimBlock>>>(zed_in,  step, particle_scale,
+                                                 particle_grid_dimension,
+                                                 particle_width, particle_height,
+                                                 dev_global_weight_memory,
+                                                 debug_img_out);
+
+    HANDLE_CUDA_ERROR(cudaUnbindTexture(particle_grid_texture_ref));
+}
