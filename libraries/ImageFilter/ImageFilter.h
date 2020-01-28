37437ffed328c24ef38572880af61a01315f6704
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-01-28 15:59:07 +0100

    [Impl] Normalize bgr values

--- libraries/ImageFilter/ImageFilter.h
+++ libraries/ImageFilter/ImageFilter.h
@@ -14,6 +14,7 @@ namespace kernel
     void meanFilter(sl::uchar1 *d_in, sl::uchar1 *d_out, int width, int height, size_t step, int radius);
     void sobelFilter(sl::uchar1 *d_in, sl::uchar1 *d_out, int width, int height, size_t step);
     void simpleRedDetector(sl::uchar4 *d_in, sl::uchar4 *d_out, unsigned char threshold, int width, int height, size_t step);
+    void eliminateLight(const sl::Mat &in_zed, sl::Mat &out_zed);
 }
 
 #endif //MODEL_BASED_TRACKER_FILTER_H
