0a5040b6f9a1ba786c16eb8c0bd11a8302ad2344
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-01-30 22:57:04 +0100

    [Impl] Constructor and initalization for particles

---
+++ libraries/ModelTracker/ParticleGrid.cpp
@@ -0,0 +1,81 @@
+#include "ParticleGrid.h"
+
+using namespace mt;
+
+ParticleGrid::ParticleGrid(std::string path_to_model, int particle_width, int particle_height, int particle_count)
+{
+    srand(time(0)); // Set a seed for evey new instantiated ParticleGenerator
+
+    if ((int)std::pow((int)std::sqrt(particle_count), 2) != particle_count) {
+        throw std::invalid_argument("[mt::ParticleGenerator::ParticleGenerator] Invalid Particle Count. "
+                                    "Make sure that it is a result of a square operation.");
+    }
+    // Set Parameters
+    m_model = new CVK::Node("model", path_to_model);
+    m_particle_grid_dimension = (int)std::sqrt(particle_count);
+
+    // Set Shader
+    m_shader_simple = new ShaderSimple( VERTEX_SHADER_BIT|FRAGMENT_SHADER_BIT, m_shader_simple_paths);
+
+    // Set Matrices
+    m_view_matrix = glm::lookAt(glm::vec3(0.0, 0.0, 25.0f), glm::vec3(0.0f, 0.0, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    m_projection_matrix = glm::perspective(glm::radians(40.0f), (float) particle_width/particle_height, 1.0f, 100.0f);
+
+    // Set Matrix Handler
+    m_view_matrix_handler = glGetUniformLocation(m_shader_simple->getProgramID(), "viewMatrix");
+    m_projection_matrix_handler = glGetUniformLocation(m_shader_simple->getProgramID(), "projectionMatrix");
+
+    // Set FBOs
+    m_color_fbo = new CVK::FBO(m_particle_grid_dimension * particle_width, m_particle_grid_dimension * particle_height, 1, true);
+
+    // Init Particles
+    initializeParticles(particle_count, particle_width, particle_height);
+}
+
+/**
+ * Bind the Color FBO and iterate over the ParticleGrid structure with an view port.
+ * Render at every position one particle.
+ */
+void ParticleGrid::renderColorTexture()
+{
+    m_color_fbo->bind();
+    CVK::State::getInstance()->setShader(m_shader_simple);
+    m_shader_simple->useProgram();
+    glUniformMatrix4fv(m_view_matrix_handler, 1, GL_FALSE, value_ptr(m_view_matrix));
+    glUniformMatrix4fv(m_projection_matrix_handler, 1, GL_FALSE, value_ptr(m_projection_matrix));
+
+    int width = m_particles[0].getWidth();
+    int height = m_particles[0].getHeight();
+
+    int i = 0;
+    for (int x = 0; x < m_particle_grid_dimension; x++) {
+        for (int y = 0; y < m_particle_grid_dimension; y++) {
+            glViewport(width * x, height * y, width, height);
+            m_model->setModelMatrix(m_particles[0].getModelMatrix());
+            m_model->render();
+            i++;
+        }
+    }
+
+    m_color_fbo->unbind();
+}
+
+/**
+ * Retruns the texture ID of the color texture.
+ * @return color texture id
+ */
+GLint ParticleGrid::getColorTexture()
+{
+    return m_color_fbo->getColorTexture(0);
+}
+
+// *** Private Functions *** //
+
+void ParticleGrid::initializeParticles(int particle_count, int width, int height)
+{
+    for (int i = 0; i < particle_count; i++)
+    {
+        mt::Particle particle(width, height);
+        m_particles.push_back(particle);
+    }
+}
--- libraries/ModelTracker/ParticleGrid.h
+++ libraries/ModelTracker/ParticleGrid.h
@@ -1,15 +1,55 @@
 #ifndef MT_PARTICLEGRID_H
 #define MT_PARTICLEGRID_H
 
-#include "ModelTracker.h"
+#include <Shader/ShaderSimple.h>
+#include <vector>
+#include <cmath>
+#include <GL/glew.h>
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/random.hpp>
+
+#include "Particle.h"
+#include <CVK_2/CVK_Framework.h>
 
 namespace mt
 {
 
 class ParticleGrid
 {
-    std::vector<mt::Particle> particles;
-    GLuint texture;
+private:
+    // Particles and ParticleGrid Parameters
+    std::vector<mt::Particle> m_particles;
+    CVK::Node *m_model = nullptr;
+    int m_particle_grid_dimension; // particle_grid_rows == particle_gird_columns == particle_dimension
+
+    // Shader paths
+    const char *m_shader_simple_paths[2] = {SHADERS_PATH "/Simple.vert", SHADERS_PATH "/Simple.frag"};
+
+    // Shader
+    ShaderSimple *m_shader_simple = nullptr;
+
+    // Matrices
+    glm::mat4 m_view_matrix;
+    glm::mat4 m_projection_matrix;
+
+    // Matrix Handler
+    GLuint m_view_matrix_handler;
+    GLuint m_projection_matrix_handler;
+
+    // FBOs
+    CVK::FBO *m_color_fbo = nullptr;
+
+    // Private Functions
+    void initializeParticles(int particle_count, int width, int height);
+
+public:
+    ParticleGrid(std::string path_to_model, int particle_width, int particle_height, int particle_count);
+
+    // Color FBO
+    void renderColorTexture();
+    GLint getColorTexture();
 };
 
 }
