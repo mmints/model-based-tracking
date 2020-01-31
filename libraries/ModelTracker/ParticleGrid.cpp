70ac2031288a66785d8cf9a4c85c9c5aa052eef4
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-01-31 13:38:16 +0100

    [Add] Normal texture rendering

--- libraries/ModelTracker/ParticleGrid.cpp
+++ libraries/ModelTracker/ParticleGrid.cpp
@@ -16,17 +16,23 @@ ParticleGrid::ParticleGrid(std::string path_to_model, int particle_width, int pa
 
     // Set Shader
     m_color_shader = new ShaderSimple( VERTEX_SHADER_BIT|FRAGMENT_SHADER_BIT, m_color_shader_paths);
+    m_normals_shader = new ShaderSimple( VERTEX_SHADER_BIT|FRAGMENT_SHADER_BIT, m_normals_shader_paths);
+
 
     // Set Matrices
     m_view_matrix = glm::lookAt(glm::vec3(0.0, 0.0, 25.0f), glm::vec3(0.0f, 0.0, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     m_projection_matrix = glm::perspective(glm::radians(40.0f), (float) particle_width/particle_height, 1.0f, 100.0f);
 
     // Set Matrix Handler
-    m_view_matrix_handler = glGetUniformLocation(m_color_shader->getProgramID(), "viewMatrix");
-    m_projection_matrix_handler = glGetUniformLocation(m_color_shader->getProgramID(), "projectionMatrix");
+    m_view_matrix_handler_color = glGetUniformLocation(m_color_shader->getProgramID(), "viewMatrix");
+    m_projection_matrix_handler_color = glGetUniformLocation(m_color_shader->getProgramID(), "projectionMatrix");
+
+    m_view_matrix_handler_normals = glGetUniformLocation(m_normals_shader->getProgramID(), "viewMatrix");
+    m_projection_matrix_handler_normals = glGetUniformLocation(m_normals_shader->getProgramID(), "projectionMatrix");
 
     // Set FBOs
     m_color_fbo = new CVK::FBO(m_particle_grid_dimension * particle_width, m_particle_grid_dimension * particle_height, 1, true);
+    m_normals_fbo = new CVK::FBO(m_particle_grid_dimension * particle_width, m_particle_grid_dimension * particle_height, 1, true);
 
     // Init Particles
     initializeParticles(particle_count, particle_width, particle_height);
@@ -39,8 +45,9 @@ void ParticleGrid::renderColorTexture()
     m_color_fbo->bind();
     CVK::State::getInstance()->setShader(m_color_shader);
     m_color_shader->useProgram();
-    glUniformMatrix4fv(m_view_matrix_handler, 1, GL_FALSE, value_ptr(m_view_matrix));
-    glUniformMatrix4fv(m_projection_matrix_handler, 1, GL_FALSE, value_ptr(m_projection_matrix));
+
+    glUniformMatrix4fv(m_view_matrix_handler_color, 1, GL_FALSE, value_ptr(m_view_matrix));
+    glUniformMatrix4fv(m_projection_matrix_handler_color, 1, GL_FALSE, value_ptr(m_projection_matrix));
 
     renderParticleGrid();
 
@@ -50,6 +57,27 @@ void ParticleGrid::renderColorTexture()
 GLint ParticleGrid::getColorTexture()
 {
     return m_color_fbo->getColorTexture(0);
+}
+
+// *** Normal FBO *** //
+
+void ParticleGrid::renderNormalTexture()
+{
+    m_normals_fbo->bind();
+    CVK::State::getInstance()->setShader(m_normals_shader);
+    m_normals_shader->useProgram();
+
+    glUniformMatrix4fv(m_view_matrix_handler_normals, 1, GL_FALSE, value_ptr(m_view_matrix));
+    glUniformMatrix4fv(m_projection_matrix_handler_normals, 1, GL_FALSE, value_ptr(m_projection_matrix));
+
+    renderParticleGrid();
+
+    m_normals_fbo->unbind();
+}
+
+GLint ParticleGrid::getNormalTexture()
+{
+    return m_normals_fbo->getColorTexture(0);
 }
 
 // *** Private Functions *** //
@@ -69,8 +97,10 @@ void ParticleGrid::renderParticleGrid()
     int height = m_particles[0].getHeight();
 
     int i = 0;
-    for (int x = 0; x < m_particle_grid_dimension; x++) {
-        for (int y = 0; y < m_particle_grid_dimension; y++) {
+    for (int x = 0; x < m_particle_grid_dimension; x++)
+    {
+        for (int y = 0; y < m_particle_grid_dimension; y++)
+        {
             glViewport(width * x, height * y, width, height);
             m_model->setModelMatrix(m_particles[0].getModelMatrix());
             m_model->render();
--- libraries/ModelTracker/ParticleGrid.h
+++ libraries/ModelTracker/ParticleGrid.h
@@ -26,20 +26,25 @@ private:
 
     // Shader paths
     const char *m_color_shader_paths[2] = {SHADERS_PATH "/Simple.vert", SHADERS_PATH "/Simple.frag"};
+    const char *m_normals_shader_paths[2] = {SHADERS_PATH "/PassNormals.vert", SHADERS_PATH "/PassNormals.frag"};
 
     // Shader
     ShaderSimple *m_color_shader = nullptr;
+    ShaderSimple *m_normals_shader = nullptr;
 
     // Matrices
     glm::mat4 m_view_matrix;
     glm::mat4 m_projection_matrix;
 
     // Matrix Handler
-    GLuint m_view_matrix_handler;
-    GLuint m_projection_matrix_handler;
+    GLuint m_view_matrix_handler_color;
+    GLuint m_projection_matrix_handler_color;
+    GLuint m_view_matrix_handler_normals;
+    GLuint m_projection_matrix_handler_normals;
 
     // FBOs
     CVK::FBO *m_color_fbo = nullptr;
+    CVK::FBO *m_normals_fbo = nullptr;
 
     // Private Functions
     void initializeParticles(int particle_count, int width, int height);
@@ -59,6 +64,18 @@ public:
      * @return color texture id
      */
     GLint getColorTexture();
+
+    /**
+     * Bind the Normal FBO and render the particle grid with the
+     * normals shader into this buffer.
+     */
+    void renderNormalTexture();
+
+    /**
+     * Retruns the texture ID of the color texture.
+     * @return color texture id
+     */
+    GLint getNormalTexture();
 };
 
 }
