edfb6e031c68ebf89013459ec91552833603ab10
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-02-04 02:38:21 +0100

    [Finalize] Finish refactoring and clean up Class

--- libraries/ModelTracker/ZedAdapter.cpp
+++ libraries/ModelTracker/ZedAdapter.cpp
@@ -1,14 +1,23 @@
 #include "ZedAdapter.h"
 
-mt::ZedAdapter::ZedAdapter(int width, int height)
+mt::ZedAdapter::ZedAdapter(Camera &zed, RESOLUTION resolution)
 {
-    m_width = width;
-    m_height = height;
+    setResolution(resolution);
+    printf("Initialize ZED with resolution: %i x %i \n", m_width, m_height);
 
-    m_texture_shader = new CVK::ShaderSimpleTexture( VERTEX_SHADER_BIT|FRAGMENT_SHADER_BIT, m_texture_shader_paths);
-    generateGlTexture(m_display_texture, width, height);
-    HANDLE_CUDA_ERROR(cudaGraphicsGLRegisterImage(&m_texture_resource, m_display_texture, GL_TEXTURE_2D, cudaGraphicsRegisterFlagsWriteDiscard));
+    initHardwareCamera(zed, resolution);
+    initTextureInteroperation();
 }
+
+mt::ZedAdapter::ZedAdapter(Camera &zed, RESOLUTION resolution, const char* path_to_svo_file)
+{
+    setResolution(resolution);
+    printf("Initialize ZED with resolution: %i x %i \n", m_width, m_height);
+
+    initSVOZedCamera(zed, path_to_svo_file);
+    initTextureInteroperation();
+}
+
 
 void mt::ZedAdapter::imageToGlTexture(Mat &zed_img)
 {
@@ -28,46 +37,46 @@ void mt::ZedAdapter::renderImage()
     m_texture_shader->renderZED();
 }
 
-void mt::ZedAdapter::displayImage(Mat &zed_img)
+// *** Private Functions *** //
+
+void mt::ZedAdapter::initTextureInteroperation()
 {
-    imageToGlTexture(zed_img);
-    renderImage();
+    m_texture_shader = new CVK::ShaderSimpleTexture( VERTEX_SHADER_BIT|FRAGMENT_SHADER_BIT, m_texture_shader_paths);
+    generateGlTexture(m_display_texture, m_width, m_height);
+    HANDLE_CUDA_ERROR(cudaGraphicsGLRegisterImage(&m_texture_resource, m_display_texture, GL_TEXTURE_2D, cudaGraphicsRegisterFlagsWriteDiscard));
 }
 
+void mt::ZedAdapter::setResolution(RESOLUTION res)
+{
+    switch(res)
+    {
+        case RESOLUTION_HD720:  m_width = 1280; m_height = 720; break;
+        case RESOLUTION_HD1080: m_width = 1920; m_height = 1080; break;
+        case RESOLUTION_HD2K:   m_width = 2208; m_height = 1242; break;
+        case RESOLUTION_VGA:    m_width = 672; m_height = 376; break;
+    }
+}
 
-void mt::ZedAdapter::initBasicZedCameraHD720(sl::Camera &zed)
+void mt::ZedAdapter::initHardwareCamera(sl::Camera &zed, RESOLUTION res)
 {
     // Init ZED Camera
     sl::InitParameters init_parameters;
-    init_parameters.camera_resolution = sl::RESOLUTION_HD720;
-    init_parameters.camera_fps = 30.f;
+    init_parameters.camera_resolution = res;
+    init_parameters.camera_fps = 60.f;
+    init_parameters.depth_mode = sl::DEPTH_MODE_PERFORMANCE;
     zed.open(init_parameters);
 }
 
 void mt::ZedAdapter::initSVOZedCamera(sl::Camera &zed, const char* path_to_file)
 {
     // Init ZED Camera from SVO files
-    sl::InitParameters initParameters;
-    initParameters.svo_input_filename.set(path_to_file);
-    initParameters.depth_mode = sl::DEPTH_MODE_PERFORMANCE;
-    sl::ERROR_CODE err = zed.open(initParameters);
+    sl::InitParameters init_parameters;
+    init_parameters.svo_input_filename.set(path_to_file);
+    init_parameters.depth_mode = sl::DEPTH_MODE_PERFORMANCE;
+    sl::ERROR_CODE err = zed.open(init_parameters);
 
     // ERRCODE display
     if (err != sl::SUCCESS) {
         zed.close();
     }
-}
-
-void mt::ZedAdapter::initZedCamera(sl::Camera &zed, const char *path_to_file)
-{
-
-    if (path_to_file) {
-        printf("##### Load from file \n");
-        initSVOZedCamera(zed, path_to_file);
-    }
-    else {
-        printf("###### Load Camera \n");
-        initBasicZedCameraHD720(zed);
-    }
-
 }
--- libraries/ModelTracker/ZedAdapter.h
+++ libraries/ModelTracker/ZedAdapter.h
@@ -15,35 +15,74 @@ using namespace sl;
 namespace mt // model based tracking
 {
 
-class ZedAdapter
-{
+    class ZedAdapter
+    {
 
-private:
-    // Parameters
-    int m_width;
-    int m_height;
+    private:
+        // Parameters
+        int m_width;
+        int m_height;
 
-    // Cuda resources for CUDA-OpenGL interoperability
-    cudaGraphicsResource* m_texture_resource;
+        // GL Texture and Shader for Rendering and Cuda resource for CUDA-OpenGL interoperability
+        const char *m_texture_shader_paths[2] = { SHADERS_PATH "/ScreenFill.vert", SHADERS_PATH "/SimpleTexture.frag"};
+        CVK::ShaderSimpleTexture *m_texture_shader;
+        cudaGraphicsResource* m_texture_resource;
+        cudaArray_t m_texture_array;
+        GLuint m_display_texture;
 
-    // GL Texture and Shader for Rendering
-    const char *m_texture_shader_paths[2] = { SHADERS_PATH "/ScreenFill.vert", SHADERS_PATH "/SimpleTexture.frag"};
-    CVK::ShaderSimpleTexture *m_texture_shader;
-    cudaArray_t m_texture_array;
-    GLuint m_display_texture;
+        // Private Functions
+        /**
+         * Set the resolution of the ZED Camera and of the GL texture
+         * @param res Resolution type from sl::RESOLUTION
+         */
+        void setResolution(RESOLUTION res);
 
-public:
-    ZedAdapter(int width, int height);
+        /**
+         * Initialize shader, generate GL texture und register CUDA resource
+         */
+        void initTextureInteroperation();
 
-    void initZedCamera(sl::Camera &zed, const char* path_to_file);
-    void initBasicZedCameraHD720(sl::Camera &zed);
-    void initSVOZedCamera(sl::Camera &zed, const char* path_to_file);
+        /**
+         * Initiate Hardware ZED Camera with a basic parameter preset.
+         * @param zed Reference to sl::Camera
+         * @param res Resolution type from sl::RESOLUTION
+         */
+        void initHardwareCamera(sl::Camera &zed, RESOLUTION res);
 
-    void imageToGlTexture(Mat &zed_img);
-    void renderImage();
+        /**
+         * Initiate ZED Camera from SVO file with a basic parameter preset.
+         * @param zed Reference to sl::Camera
+         * @param res Resolution type from sl::RESOLUTION
+         */
+        void initSVOZedCamera(sl::Camera &zed, const char* path_to_file);
 
-    void displayImage(Mat &zed_img);
-};
+    public:
+        /**
+         * Constructor for Hardware Camera Adapter
+         * @param zed Reference to sl::Camera
+         * @param res Resolution type from sl::RESOLUTION
+         */
+        ZedAdapter(Camera &zed, RESOLUTION resolution);
+
+        /**
+         * Constructor for SVO file Camera Adapter
+         * @param zed Reference to sl::Camera
+         * @param res Resolution type from sl::RESOLUTION
+         * @param path_to_svo_file path to file location
+         */
+        ZedAdapter(Camera &zed, RESOLUTION resolution, const char* path_to_svo_file);
+
+        /**
+         * Transfer a sl::Mat saved on GPU into a GL texture
+         * @param zed_img sl::Mat image
+         */
+        void imageToGlTexture(Mat &zed_img);
+
+        /**
+         * Render the last generated sl::Mat
+         */
+        void renderImage();
+    };
 }
 
 #endif //MODEL_BASED_TRACKER_ZED_HELPER_H
