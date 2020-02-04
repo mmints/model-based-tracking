0d32ab586742deadca600774097c8ad5214a98b8
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-02-04 03:20:11 +0100

    [Update] Use the nre ZedAdapter in Apps

--- executables/Test_ZedAdapter/main.cpp
+++ executables/Test_ZedAdapter/main.cpp
@@ -11,18 +11,15 @@ int main(int argc, char **argv)
     window = initGLWindow(window, WIDTH, HEIGHT, "Test ZED Adapter", BLACK);
 
     Camera zed;
-    initBasicZedCameraHD720(zed);
-    //Mat zed_img;
+    Mat zed_img;
 
-    ZedAdapter zedAdapter(WIDTH, HEIGHT);
+    ZedAdapter zedAdapter(zed, RESOLUTION_HD720);
 
     while( !glfwWindowShouldClose(window))
     {
         zed.grab();
-        //zed.retrieveImage(zed_img, VIEW_LEFT, MEM_GPU);
-        //zedAdapter.imageToGlTexture(zed_img);
-        zedAdapter.retrieveImage(zed);
-        zedAdapter.imageToGlTexture();
+        zed.retrieveImage(zed_img, VIEW_LEFT, MEM_GPU);
+        zedAdapter.imageToGlTexture(zed_img);
         zedAdapter.renderImage();
 
         glfwSwapBuffers(window);
@@ -30,7 +27,7 @@ int main(int argc, char **argv)
     }
 
     // Clean up
-    zedAdapter.freeImages();
+    zed_img.free();
     zed.close();
 
     glfwDestroyWindow(window);
