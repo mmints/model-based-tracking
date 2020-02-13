12761e259751e01c05e45fb2f0a0de21e0a02d09
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-02-13 19:45:00 +0100

    [Update] Clean up exe and remove every not used feature

--- executables/Test_ParticleFilter/main.cpp
+++ executables/Test_ParticleFilter/main.cpp
@@ -42,38 +42,26 @@ int main(int argc, char **argv)
     // zed interopertion
     Mat img_raw  =  Mat(WIDTH, HEIGHT, MAT_TYPE_8U_C4, MEM_GPU);
     Mat img_rgb =  Mat(WIDTH, HEIGHT, MAT_TYPE_8U_C4, MEM_GPU);
-    Mat img_depth =  Mat(WIDTH, HEIGHT, MAT_TYPE_8U_C4, MEM_GPU);
-    Mat img_normals =  Mat(WIDTH, HEIGHT, MAT_TYPE_8U_C4, MEM_GPU);
-
-
-    Mat img_out =  Mat(WIDTH, HEIGHT, MAT_TYPE_8U_C4, MEM_GPU); // Only for the testing Kernel
 
     int measure_count = 0;
     while(!glfwWindowShouldClose( window))
     {
-        particleGrid.update(0.1f, .2f);
+        particleGrid.update(0.1f, .1f);
         particleGrid.renderColorTexture();
-        particleGrid.renderDepthTexture();
-        particleGrid.renderNormalTexture();
 
         zed.grab();
         HANDLE_ZED_ERROR(zed.retrieveImage(img_raw, VIEW_LEFT, MEM_GPU));
-        HANDLE_ZED_ERROR(zed.retrieveImage(img_depth, VIEW_DEPTH, MEM_GPU));        // MEASURE_DEPTH needs sl::MAT_TYPE_32F_C1
-        HANDLE_ZED_ERROR(zed.retrieveImage(img_normals, VIEW_NORMALS, MEM_GPU));    // MEASURE_NORMALS needs sl::MAT_TYPE_32F_C4
 
         filter::convertBGRtoRGB(img_raw, img_rgb);
 
         zedAdapter.imageToGlTexture(img_rgb);
         zedAdapter.renderImage();
 
-        // Calculate the weights
+        // Particle Filter routine
         particleFilter.calculateWeightColor(img_rgb, particleGrid);
-        particleFilter.calculateWeightDepth(img_depth, particleGrid);
-        particleFilter.calculateWeightNormals(img_normals, particleGrid);
-
         particleFilter.setParticleWeight(particleGrid);
-
         particleFilter.resample(particleGrid, 60);
+
         particleGrid.renderFirstParticleToScreen();
 
         glfwSwapBuffers( window);
@@ -85,9 +73,6 @@ int main(int argc, char **argv)
     printf("CLEAN UP... \n");
     img_raw.free();
     img_rgb.free();
-    img_depth.free();
-    img_normals.free();
-    img_out.free();
     zed.close();
     HANDLE_CUDA_ERROR(cudaDeviceReset());
     printf("DONE \n");
