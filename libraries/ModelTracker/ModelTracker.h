babe1ff38a38aeabf3fc16ea86ef6060f9a8db31
Mark Oliver Mints <mmints@uni-koblenz.de>
2020-02-23 13:41:24 +0100

    [Add] Likelihood types to library header

--- libraries/ModelTracker/ModelTracker.h
+++ libraries/ModelTracker/ModelTracker.h
@@ -1,12 +1,13 @@
 #ifndef MODELTRACKER_H
 #define MODELTRACKER_H
 
+#include "helper_functions.h"
+#include "kernel_functions.h"
+#include "likelihood_types.h"
+
 #include "Particle.h"
 #include "ZedAdapter.h"
 #include "ParticleGrid.h"
 #include "ParticleFilter.h"
-
-#include "helper_functions.h"
-#include "kernel_functions.h"
 
 #endif //MODELTRACKER_H
